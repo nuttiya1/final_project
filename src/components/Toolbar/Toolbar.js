ab88930bbe8f37a57b2efac122cf152ed356bf7a
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2019-04-29 23:45:17 +0700

    Hide slide component

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -6,9 +6,9 @@ import './Toolbar.css';
 const toolbar = props => (
   <header className="toolbar">
     <nav className="toolbar__navigation">
-        <div className="toolbar__toggle-button">
+        {/* <div className="toolbar__toggle-button">
             <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
+        </div> */}
         <div className="toolbar__logo"><a href="/">Bangkok Traffic Prediction</a></div>
         <div className="spacer" />
     </nav>
